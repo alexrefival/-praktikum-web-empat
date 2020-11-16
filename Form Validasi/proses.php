749d02039bc8404d34a8f2f9b286816f5b3a7cd5
Adhitya Alvianzyah <72731352+alexrefival@users.noreply.github.com>
2020-11-16 12:12:18 -0800

    ADHITYA ALVIANZYAH_0110220276_TI 06 WEEKEND
    ADHITYA ALVIANZYAH_0110220276_TI 06 WEEKEND

---
+++ Form Validasi/proses.php
@@ -0,0 +1,19 @@
+<?php
+   echo "DATA PELANGGAN";
+   echo "<br />";
+   echo "<br />";
+   echo "NAMA : ";
+   echo $_GET['nama'];
+   echo "<br />";
+   echo "EMAIL : ";
+   echo $_GET['email'];
+   echo "<br />";
+   echo "JAM KEBERANGKATAN : ";
+   echo $_GET['otw'];
+   echo "<br />";
+   echo "TUJUAN KEBERANGKATAN : ";
+   echo $_GET['tujuan'];
+   echo "<br />";
+   echo "JUMLAH TIKET : ";
+   echo $_GET['tiket'];
+?>
